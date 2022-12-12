6b8cedb4f25781ede6286fce607004175eca66ec
Gabriel Renard <gabriel.renard@protonmail.com>
2022-12-12 21:18:05 +0100

    Add projects data to constants folder

---
+++ constants/index.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    id: "project1",
+    title: "Project 1",
+    description: "This is a project",
+    imageUrl: "/images/project1.png",
+  },
+  {
+    id: "project1",
+    title: "Project 1",
+    description: "This is a project",
+    imageUrl: "/images/project1.png",
+  },
+  {
+    id: "project1",
+    title: "Project 1",
+    description: "This is a project",
+    imageUrl: "/images/project1.png",
+  },
+];
