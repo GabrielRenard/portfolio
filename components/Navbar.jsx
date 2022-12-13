0163477f1702013a5c9eaaa9741828da5bd5f463
Gabriel Renard <gabriel.renard@protonmail.com>
2022-12-13 15:54:16 +0100

    create About section without animations

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,21 +10,23 @@ const Navbar = () => {
       whileInView="show"
       className="hidden md:flex relative py-8 w-11/12 md:w-9/12 lg:w-7/12 mx-auto justify-between"
     >
-      <div className="absolute w-[50%] inset-0 gradient-01" />
+      <div className="absolute w-full inset-0 gradient-01" />
       <div className="absolute  inset-0 flex justify-between mx-auto items-center ">
         <div>
           <a href="#home" className="text-3xl">
             GR
           </a>
         </div>
-        <ul className="flex sm:space-x-[3rem] md:space-x-[3rem] lg:space-x-[4rem]">
+        <ul className="flex sm:space-x-[3rem] md:space-x-[3rem] lg:space-x-[4rem] lg:text-xl">
           <a href="#home">Home</a>
           <a href="#about">About</a>
           <a href="#skills">Skills</a>
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
         </ul>
-        <ImSun size="1.5rem" className="cursor-pointer" />
+        <a href="#about">
+          <ImSun size="1.5rem" className="cursor-pointer" />
+        </a>
       </div>
     </motion.nav>
   );
--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import Image from "next/image";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h1 className="text-4xl text-center mt-[5rem]">About Me</h1>
+      <h2 className="text-xl text-center mt-2 mb-[5rem]">My Introduction</h2>
+      <div className="grid md:grid-cols-2 lg:max-w-[1000px] mx-auto items-center place-items-center gap-[3rem]">
+        <Image
+          src={"/Images/profile2.jpg"}
+          width={400}
+          height={400}
+          alt="profile image"
+          className="rounded-xl"
+        />
+
+        <div className="grid md:grid-rows-2 px-[4rem]">
+          <p className="text-center text-2xl self-center">
+            I am looking for an entry level position in full-stack web
+            development.
+          </p>
+          <div className="flex flex-col my-[4rem] md:my-0">
+            <div className="flex justify-around text-center">
+              <div className="flex flex-col">
+                <span className="text-4xl font-extrabold">1+</span>
+                <span>
+                  Years
+                  <br />
+                  Experience
+                </span>
+              </div>
+              <div className="flex flex-col">
+                <span className="text-4xl font-extrabold">10+</span>
+                <span>
+                  Completed
+                  <br />
+                  Projects
+                </span>
+              </div>
+            </div>
+            <button className="bg-yellow-500 text-zinc-900 w-full sm:w-1/3 md:w-8/12 self-center p-4 rounded-full mt-[4rem] font-bold">
+              Download CV
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
