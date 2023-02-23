ed180043d13040ebe7dc5392417b7e14428fbd04
Gabriel Renard <gabriel.renard@protonmail.com>
2023-02-23 13:36:54 +0100

    Add gtag code in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,35 @@
-import {Analytics} from '@vercel/analytics/react';
 import "../styles/globals.css";
 import Layout from "../Layout/Layout";
+import Script from "next/script";
 
 import localFont from "@next/font/local";
 const Jiro = localFont({ src: "./Jiro.ttf", variable: "--Jiro" });
 
 function MyApp({ Component, pageProps }) {
+
   return (
-    <main className={`${Jiro.variable}`}>
-      <Layout>
-        <Component {...pageProps} />
-        <Analytics />
-      </Layout>
-    </main>
+    <>
+    <Script 
+    strategy="lazyOnload"
+    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+
+    <Script strategy="lazyOnload">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+      `}
+    </Script>
+
+      <main className={`${Jiro.variable}`}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,30 @@
 import Head from "next/head";
-import Image from "next/image";
+import Script from "next/script";
 import { Hero, About, Projects, Contact, Skills } from "../sections";
-import styles from "../styles";
 
 export default function Home() {
   return (
-    <div className="overflow-hidden ">
-      <Hero />
-      <div className="relative">
-        <div className="absolute inset-0 about-gradient" />
-        <About />
+    <>
+      <Head>
+        <title>Gabriel Renard Portfolio</title>
+        <meta name="description" content="My personal portfolio" />
+      </Head>
+      <div className="overflow-hidden ">
+        <Hero />
+        <div className="relative">
+          <div className="absolute inset-0 about-gradient" />
+          <About />
+        </div>
+        <div className="relative">
+          <div className="gradient-02 z-0" />
+          <Skills />
+        </div>
+        <div className="relative">
+          <div className="gradient-03 z-0" />
+          <Projects />
+          <Contact />
+        </div>
       </div>
-      <div className="relative">
-        <div className="gradient-02 z-0" />
-        <Skills />
-      </div>
-      <div className="relative">
-        <div className="gradient-03 z-0" />
-        <Projects />
-        <Contact />
-      </div>
-    </div>
+    </>
   );
 }
